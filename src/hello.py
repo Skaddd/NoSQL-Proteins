354a4801819fd1933d9a51148057017d1ae20248
Mateo Lebrun <mateo.lebrun@telecomnancy.net>
2020-12-14 19:07:56 +0100

    final version v0.1

--- src/hello.py
+++ src/hello.py
@@ -26,6 +26,9 @@ def hello_world():
         doLink = True if request.form.get('idlink') is not None else False
         jaccardTreshold = request.form['jaccardtreshold']
         create_req(humans_cleared,pid,pname,session)
+        cypher_query=get_request(pid,pname,doLink,jaccardTreshold)
+        replace_line(get_path()+'/templates/graph.html', 33, '                initial_cypher: "'+cypher_query+'",\n')
+
         return graph()
     
     
@@ -38,11 +41,46 @@ def graph():
 
 
 
+def replace_line(file_name, line_num, text):
+    lines = open(file_name, 'r').readlines()
+    lines[line_num] = text
+    out = open(file_name, 'w')
+    out.writelines(lines)
+    out.close()
+
+def get_request(pid,pid2,doLink,jaccardTreshold):
+    if pid2=="":
+        string="MATCH p=(p1:Prot{prot_id:'"+pid+"'})-[j:LINK]-(p2:Prot) where toFloat(j.jaccard)>"+jaccardTreshold+" return p LIMIT 100"
+    else:
+        if doLink:
+            string="MATCH p=(p1:Prot) where p1.prot_id in['"+pid+"','"+pid2+"'] return p"
+        else:
+            string="MATCH p=(p1:Prot)-[j:LINK]-(p2:Prot)where p1.prot_id='"+pid+"' return p LIMIT 30 UNION MATCH p=(p1:Prot)-[j:LINK]-(p2:Prot) where p1.prot_id='"+pid2+"' return p LIMIT 30"
+
+    return string
+
+
+
+
+
 if __name__ == '__main__':
     session=connection_dbms() #1
     app = Flask(__name__)
     app.register_blueprint(hello, url_prefix='/')
     app.run(host='0.0.0.0', port='8000')
 
-    
-    
+
+
+#MATCH p=(p1:Prot{prot_id:'A0A075B6H9'})-[j:LINK]-(p2:Prot) where toFloat(j.jaccard)> 0.3 return p
+
+
+#affiche le lien si ya et si ya pas np
+#MATCH p=(p1:Prot) where p1.prot_id in['A0A075B6H9','P29016'] return p
+
+
+
+
+
+
+#MATCH p=(p1:Prot)-[j:LINK]-(p2:Prot) where p1.prot_id='A0A075B6H9' return p LIMIT 30 UNION
+#MATCH p=(p1:Prot)-[j:LINK]-(p2:Prot) where p1.prot_id='P29016' return p LIMIT 30
--- src/test.py
+++ src/test.py
@@ -7,8 +7,7 @@
 
 
 if __name__ == '__main__':
-	a=""
-	if a!="":
-		print("OUIII")
-	else :
-		print("NON")
+	pid="LALALA"
+	pid2="NONONON"
+	string="MATCH p=(p1:Prot)-[j:LINK]-(p2:Prot)where p1.prot_id='"+pid+"' return p LIMIT 30 UNION MATCH p=(p1:Prot)-[j:LINK]-(p2:Prot) where p1.prot_id='"+pid2+"' return p LIMIT 30"
+	print(string)
